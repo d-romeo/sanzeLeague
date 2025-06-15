408557ce3e2e5a126e10eb1be376e5437c8dcd06
Daniele Romeo <danieleromeo.ele@gmail.com>
2025-06-15 18:31:06 +0200

    Escludi api/db.php dal controllo di versione

--- api/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = 'localhost'; 
-    $db = 'u908685741_sanze';
-    $user = 'u908685741_sanze';
-    $pass = 'Sanzeleague25!';
-
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
